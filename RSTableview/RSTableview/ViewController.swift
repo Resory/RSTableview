02c928a0cc65d0b2a32572896e75884a997a5706
liang_zhong_rong <zhongrong.liang@billionsfinance.cn>
2015-12-27 20:47:38 +0800

    init

---
+++ RSTableview/RSTableview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSTableview
+//
+//  Created by Resory on 15/12/27.
+//  Copyright © 2015年 Resory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
